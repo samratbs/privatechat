ce0189d376643355e129bcdcb4a6e0d2d60f9eba
Samrat Shah <samratbs76@gmail.com>
2022-12-21 23:11:17 +0545

    Initial Project Setup and Basic Chat Layout

---
+++ PrivateChat.MAUI.Client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrivateChat.MAUI.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
