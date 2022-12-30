0e4356db8fdb0192288d452bf9f52713ccae15ef
Samrat Shah <samratbs76@gmail.com>
2022-12-31 00:02:58 +0545

    Finish Login and Registration System. Add MudBlazor Services.

---
+++ PrivateChat.Domains/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateChat.Domains.Responses
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public bool Found  { get; set; }
+    }
+}
